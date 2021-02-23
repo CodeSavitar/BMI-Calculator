b91638e15cf9c5356d7b399c9a6509695eb73922
Adhith Sankar <adhithsagit0510@gmail.com>
2021-02-23 13:31:33 +0530

    change4

--- myapp/app.py
+++ myapp/app.py
@@ -8,7 +8,8 @@ def calc():
     bmi=''
     if request.method == 'POST' and 'weight' in request.form and 'height' in request.form:
         bmi=''
+        Name = str(request.form.get('name'))
         Weight = float(request.form.get('weight'))
         Height = float(request.form.get('height'))
         bmi = round(Weight/((Height/100)**2),2)
-    return render_template("index.html",bmi=bmi)    
+    return render_template("index.html",bmi=bmi,Name=Name)    
