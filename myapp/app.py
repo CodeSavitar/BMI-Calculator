3b6501a9418f9da54c2db20c609b67c057147d1b
Adhith Sankar <adhithsagit0510@gmail.com>
2021-02-23 18:45:58 +0530

    change4

--- myapp/app.py
+++ myapp/app.py
@@ -5,10 +5,10 @@ app = Flask(__name__)
 @app.route("/",methods = ['POST','GET'])
 
 def calc():
-    bmi=''
+    bmi = 0.0
+    Name=''
     if request.method == 'POST' and 'weight' in request.form and 'height' in request.form:
-        bmi=''
-        Name = str(request.form.get('name'))
+        Name = str(request.form.get('Name'))
         Weight = float(request.form.get('weight'))
         Height = float(request.form.get('height'))
         bmi = round(Weight/((Height/100)**2),2)
